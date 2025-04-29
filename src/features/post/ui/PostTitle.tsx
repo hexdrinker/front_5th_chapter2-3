5ba0c51469e7aa530ddafa393f3dd9a0803779ff
Park YuongHo <hexdrinker2020@gmail.com>
2025-04-30 01:27:53 +0900

    refactor(post): PostTitle 컴포넌트 리팩토링

--- src/features/post/ui/PostTitle.tsx
+++ src/features/post/ui/PostTitle.tsx
@@ -1,22 +1,27 @@
 import { highlightText } from "@/shared/lib/helpers"
 import TagItem from "@/features/post/ui/TagItem"
+import { useAtom } from "jotai"
+import { selectedTagAtom } from "@/entities/tag/model/store"
 
 interface PostTitleProps {
   post: {
     title: string
     tags: string[]
   }
-  selectedTag: string
   searchQuery: string
 }
 
-const PostTitle = ({ post, selectedTag, searchQuery }: PostTitleProps) => {
+const PostTitle = ({ post, searchQuery }: PostTitleProps) => {
+  const [selectedTag, setSelectedTag] = useAtom(selectedTagAtom)
+
   return (
     <div className="space-y-1">
       <div>{highlightText(post.title, searchQuery)}</div>
 
       <div className="flex flex-wrap gap-1">
-        {post.tags?.map((tag) => <TagItem key={tag} tag={tag} onClick={() => {}} selectedTag={selectedTag} />)}
+        {post.tags?.map((tag) => (
+          <TagItem key={tag} tag={tag} onClick={() => setSelectedTag(tag)} selectedTag={selectedTag} />
+        ))}
       </div>
     </div>
   )
