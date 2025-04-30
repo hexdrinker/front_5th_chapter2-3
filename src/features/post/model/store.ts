b91393c3aa6bafc1620295012832e553396a45b2
Park YuongHo <hexdrinker2020@gmail.com>
2025-05-01 03:35:41 +0900

    feat(post): 필터링, 검색을 위한 search params 상태 추가

--- src/features/post/model/store.ts
+++ src/features/post/model/store.ts
@@ -4,3 +4,11 @@ export const showAddDialogAtom = atom(false)
 export const showEditDialogAtom = atom(false)
 export const showDetailDialogAtom = atom(false)
 export const newPostAtom = atom({ title: "", body: "", userId: 1 })
+export const searchParamsAtom = atom({
+  limit: 10,
+  skip: 0,
+  searchQuery: "",
+  sortBy: "none",
+  sortOrder: "asc",
+  tag: "",
+})
