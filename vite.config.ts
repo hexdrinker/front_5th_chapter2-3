55294cd31f007941a62b5e893720f2079b395843
Park YuongHo <hexdrinker2020@gmail.com>
2025-05-01 16:04:27 +0900

    chore: gh-pages 설치 및 빌드 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import path from "path"
+import { resolve } from "path"
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
   server: {
