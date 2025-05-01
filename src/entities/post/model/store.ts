982298d3ff893c0dc4662229e8d038c667ff77e5
Park YuongHo <hexdrinker2020@gmail.com>
2025-05-01 15:02:47 +0900

    feat(post): posts 패칭할 때의 loading 상태 atom 추가

--- src/entities/post/model/store.ts
+++ src/entities/post/model/store.ts
@@ -3,6 +3,8 @@ import type { IPostWithAuthor } from "@/entities/post/model/types"
 
 export const postsAtom = atom<IPostWithAuthor[]>([])
 
+export const postsLoadingAtom = atom<boolean>(false)
+
 export const postsTotalAtom = atom<number>(0)
 
 export const selectedPostAtom = atom<IPostWithAuthor | null>(null)
